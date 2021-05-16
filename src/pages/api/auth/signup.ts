9b9626cb8896ba8b4a4d6d92cb270aa680636849
Minya <carter850065@gmail.com>
2021-05-16 16:31:45 +0800

    fix: fix sinup functions - fix the use of try/catch with await

--- src/pages/api/auth/signup.ts
+++ src/pages/api/auth/signup.ts
@@ -7,9 +7,13 @@ import pool from 'utils/db';
 
 import { createSupplier, createBuyer } from './user';
 
+const errHandler = (promise: any) => {
+  return promise
+    .then((data: any) => [data, undefined])
+    .catch((err: any) => Promise.resolve([undefined, err]));
+};
 const checkEmaiExist = async (Email: string) => {
   let result = null;
-  // Reported unnecessary try catch wrapper
   result = await pool.query(
     `SELECT EXISTS (SELECT * FROM "User" WHERE "Email"='${Email}');`,
   );
@@ -24,58 +28,56 @@ const handler = nextConnect().post(
     try {
       exists = await checkEmaiExist(Email);
     } catch (err) {
-      res.status(500).json({ err });
+      return res.status(500).json({ err });
     }
 
     if (exists) {
-      res
+      return res
         .status(400)
         .json({ success: false, message: 'Email is already taken!' });
-      return;
     }
 
     const hashedPassword = bcrypt.hashSync(Password as string, 10);
     if (Type === 'supplier') {
       const { State_ID, City_ID } = req.body;
-      createSupplier(Name, ABN, Logo, State_ID, City_ID).then((result: any) => {
-        const SupplierID = result.rows[0].ID;
-        if (SupplierID)
-          pool
-            .query(
-              `INSERT INTO "User" ("Name", "Password", "Email", "Supplier_ID", "CreatedAt") VALUES ('${Name}', '${hashedPassword}', '${Email}', '${SupplierID}', '${moment().format(
-                'YYYY-MM-DD',
-              )}');`,
-            )
-            .then((mes: any) => {
-              if (mes) res.status(200).json({ success: true });
-            })
-            .catch((err: any) => {
-              if (err)
-                res
-                  .status(500)
-                  .json({ success: false, message: 'Something wrong' });
-            });
-      });
-    } else if (Type === 'buyer') {
-      createBuyer(Name, ABN, Logo)
-        .then((result: any) => {
-          const BuyerID = result.rows[0].ID;
+      const [SupplierID, supplierIDErr] = await errHandler(
+        createSupplier(Name, ABN, Logo, State_ID, City_ID),
+      );
+      if (supplierIDErr)
+        return res.status(500).json({ message: 'Something wrong' });
 
-          pool
-            .query(
-              `INSERT INTO "User" ("Name", "Password", "Email", "Buyer_ID", "CreatedAt") VALUES ('${Name}', '${hashedPassword}', '${Email}', '${BuyerID}', '${moment().format(
-                'YYYY-MM-DD',
-              )}');`,
-            )
-            .then((mes: any) => {
-              if (mes) res.status(200).json({ success: true });
-            });
-        })
-        .catch((err: any) => {
-          if (err)
-            res.status(500).json({ succss: false, message: 'Something wrong' });
-        });
+      try {
+        const result = await pool.query(
+          `INSERT INTO "User" ("Name", "Password", "Email", "Supplier_ID", "CreatedAt") VALUES ('${Name}', '${hashedPassword}', '${Email}', '${SupplierID}', '${moment().format(
+            'YYYY-MM-DD',
+          )}');`,
+        );
+        if (result) res.status(200).json({ success: true });
+      } catch (err) {
+        return res.status(500).json({ err });
+      }
+    } else if (Type === 'buyer') {
+      const [BuyerID, BuyerIDErr] = await errHandler(
+        createBuyer(Name, ABN, Logo),
+      );
+      if (BuyerIDErr)
+        return res
+          .status(500)
+          .json({ success: false, message: 'Something wrong' });
+      try {
+        const result = await pool.query(
+          `INSERT INTO "User" ("Name", "Password", "Email", "Buyer_ID", "CreatedAt") VALUES ('${Name}', '${hashedPassword}', '${Email}', '${BuyerID}', '${moment().format(
+            'YYYY-MM-DD',
+          )}');`,
+        );
+        if (result) return res.status(200).json({ succes: true });
+      } catch (err) {
+        return res
+          .status(500)
+          .json({ success: false, message: 'Something wrong' });
+      }
     }
+    return res.status(500).json({ message: 'something resally wrong' });
   },
 );
 
--- src/pages/api/auth/user.ts
+++ src/pages/api/auth/user.ts
@@ -1,28 +1,28 @@
 import moment from 'moment';
 import pool from 'utils/db';
 
-const createSupplier = (
+const createSupplier = async (
   Name: string,
   ABN: number,
   Logo: string,
   State_ID: number,
   City_ID: number,
 ) => {
-  const result = pool.query(
+  const result = await pool.query(
     `INSERT INTO "Supplier" ("Name", "ABN", "Logo", "State_ID", "City_ID", "CreatedAt") VALUES ('${Name}', '${ABN}', '${Logo}', '${State_ID}', '${City_ID}', '${moment().format(
       'YYYY-MM-DD',
     )}') RETURNING "ID";`,
   );
-  return result;
+  return result.rows[0].ID;
 };
 
-const createBuyer = (Name: string, ABN: number, Logo: string) => {
-  const result = pool.query(
+const createBuyer = async (Name: string, ABN: number, Logo: string) => {
+  const result = await pool.query(
     `INSERT INTO "Buyer" ("Name", "ABN", "Logo", "CreatedAt") VALUES ('${Name}', '${ABN}', '${Logo}', '${moment().format(
       'YYYY-MM-DD',
     )}') RETURNING "ID";`,
   );
-  return result;
+  return result.rows[0].ID;
 };
 
 // const createBuyer = async (
