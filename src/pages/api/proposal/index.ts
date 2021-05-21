50b05a4bea6a027f6ad7a0c46dd8eaa51317d4c2
Minya <carter850065@gmail.com>
2021-05-21 20:08:38 +0800

    feat: Add Proposal api and Tender api - add column to `TenderAttachment` - add api: GET`/api/tender`, GET`/api/tender/[id]`, POST`/api/tender` - add api: GET`/api/propsal`, GET`/api/proposal/[id]`,   POST`/api/proposal`

---
+++ src/pages/api/proposal/index.ts
@@ -0,0 +1,82 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import nextConnect from 'next-connect';
+
+import moment from 'moment';
+import pool from 'utils/db';
+
+const createProposal = async (
+  Tender_ID: number,
+  Supplier_ID: number,
+  Description: string,
+  Offer: any,
+) =>
+  pool.query(
+    `INSERT INTO "Proposal" ("Tender_ID", "Supplier_ID", "Description", "Offer", "ApprovedAt", "CreatedAt")
+     VALUES ('${Tender_ID}', '${Supplier_ID}', '${Description}', ${Offer}, '${moment().format(
+      'YYYY-MM-DD',
+    )}', '${moment().format('YYYY-MM-DD')}')
+     RETURNING "ID"`,
+  );
+
+const createProposalAttachment = async (ID: number, URL: string) =>
+  pool.query(
+    `INSERT INTO "ProposalAttachment" ("Proposal_ID", "URL")
+   VALUES ('${ID}', '${URL}') RETURNING "ID"`,
+  );
+
+const handler = nextConnect()
+  .get((req: NextApiRequest, res: NextApiResponse) => {
+    pool
+      .query(`SELECT * FROM "Proposal"`)
+      .then((result: any) => {
+        if (result.rowCount > 0)
+          return res.status(200).json({ success: true, Proposal: result.rows });
+        return res.status(400).json({
+          succes: false,
+          message: 'Something wrong when getting Proposal',
+        });
+      })
+      .catch((err: any) => {
+        return res.status(500).json({
+          success: false,
+          message: 'Something wrong with Proposal',
+          err,
+        });
+      });
+  })
+  .post(async (req: NextApiRequest, res: NextApiResponse) => {
+    const { Tender_ID, Supplier_ID, Description, Offer } = req.body;
+    let result = null;
+    try {
+      result = await createProposal(Tender_ID, Supplier_ID, Description, Offer);
+    } catch (err) {
+      return res.status(500).json({
+        success: false,
+        message: 'Something wrong when creating Proposal',
+        err,
+      });
+    }
+
+    if (result.rowCount > 0) {
+      const ProposalID = result.rows[0].ID;
+      const { ProposalAttachment } = req.body;
+      try {
+        ProposalAttachment.map(async (attachment: any) => {
+          result = await createProposalAttachment(ProposalID, attachment.URL);
+          return result;
+        });
+        return res.status(200).json({ success: true, ProposalID });
+      } catch (err) {
+        return res.status(500).json({
+          success: false,
+          message: 'Something wrong when creating attachment',
+          err,
+        });
+      }
+    }
+    return res
+      .status(500)
+      .json({ succes: false, message: 'Create Proposal not successful' });
+  });
+
+export default handler;
---
+++ src/pages/api/tender/[ID].ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import nextConnect from 'next-connect';
+
+import pool from 'utils/db';
+
+const handler = nextConnect().get(
+  (req: NextApiRequest, res: NextApiResponse) => {
+    const { ID } = req.body;
+    pool
+      .query(`SELECT * FROM "Tender" WHERE "ID"=$1`, [ID])
+      .then((result: any) => {
+        if (result.rowCount > 0)
+          return res.status(200).json({ success: true, Tender: result.rows });
+        return res
+          .status(400)
+          .json({ success: false, message: 'Tender not exist' });
+      })
+      .catch((err: any) => {
+        return res.status(500).json({
+          success: false,
+          message: 'Something wrong with Tender',
+          err,
+        });
+      });
+  },
+);
+
+export default handler;
